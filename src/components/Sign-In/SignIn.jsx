3baa0780425b66bc51db3148bdd0b74e2c34b5f0
Zaid <zaid_lorea@yahoo.com.mx>
2021-06-29 12:27:03 -0500

    Sign in form with functionality

---
+++ src/components/Sign-In/SignIn.jsx
@@ -0,0 +1,116 @@
+import React, {useState} from 'react';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import { Container,  Dialog, DialogContentText, DialogContent } from '@material-ui/core';
+import { signIn } from '../../functions/register-signin';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function SignIn() {
+  const classes = useStyles();
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [message, setMessage] = useState('')
+
+  const submit = async(e) =>{
+    e.preventDefault()
+    const response = await signIn(email, password)
+    console.log(response)
+    if (response.error) {
+      setMessage(response.error)
+      return
+    }
+    window.localStorage.setItem('ranked-token', response)  
+    window.location.href = '/home'
+  }
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Typography component="h1" variant="h5">
+          Sign In
+        </Typography>
+        <form 
+          className={classes.form} 
+          onSubmit={(e)=> submit(e)}>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required={true}
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            onChange={(e)=> setEmail(e.target.value)}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required={true}
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            onChange={(e)=> setPassword(e.target.value)}
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+            
+            </Grid>
+            <Grid item>
+              <Link href="/register" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      {message 
+      ? <Dialog open onClose={()=> setMessage(null)}>
+          <DialogContent>
+              <DialogContentText>
+                  {message}
+              </DialogContentText>
+          </DialogContent>
+      </Dialog>
+      : null}
+    </Container>
+  );
+}
