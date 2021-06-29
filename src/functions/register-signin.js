58b71090ef0a383899a0fe6dc2a6c2ed4985b08d
Zaid <zaid_lorea@yahoo.com.mx>
2021-06-29 12:25:42 -0500

    Created register and sign in functions

---
+++ src/functions/register-signin.js
@@ -0,0 +1,46 @@
+const apiURL = process.env.REACT_APP_API_URL
+
+export const register = async (first_name, last_name, username, email, password) =>{
+    
+    try {
+        const response = await fetch(`${apiURL}/user/register`, {
+            method: 'POST',
+            body: JSON.stringify({
+                first_name,
+                last_name,
+                username,
+                password,
+                email
+            }),
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+        })
+        if (response.ok) return await response.json()
+        return {error: await response.json()}
+    } catch (error) {
+        alert(error)
+    }
+}
+
+export const signIn = async (email, password) =>{
+    try {
+        const response = await fetch(`${apiURL}/user/login`, {
+            method: 'POST',
+            body: JSON.stringify({
+                email,
+                password
+            }),
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+        })
+        if (response.ok) return await response.json()
+        
+        return {error: await response.json()}
+    } catch (error) {
+        console.error(error)
+    }
+}
