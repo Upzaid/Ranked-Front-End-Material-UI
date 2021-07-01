5aee739c6ab1f61a7a55724abf6ecea026ec8de0
Zaid <zaid_lorea@yahoo.com.mx>
2021-06-30 19:08:34 -0500

    Created fuctions for creating and editing tournaments, and geting list of tournaments for the user

---
+++ src/functions/tournaments.js
@@ -0,0 +1,50 @@
+const apiURL = process.env.REACT_APP_API_URL
+
+export const createTournament = async(name, details) =>{
+    const response = await fetch(`${apiURL}/tournament/create`, {
+        method: 'POST',
+        headers: {
+            'ranked-token': localStorage.getItem('ranked-token'),
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({name, details})
+    })
+    const data = await response.json()
+    const message = data.message
+
+    return response.ok 
+    ? {message, type: 'success'} 
+    : {message, type: 'error'}
+}
+
+export const editTournament = async(tournament_uuid, name, details, status)=>{
+    const response = await fetch(`${apiURL}/tournament/${tournament_uuid}`, {
+        method: 'PUT',
+        headers: {
+            'ranked-token': localStorage.getItem('ranked-token'),
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({name, details, status})
+    })
+    const data = await response.json()
+    if (response.ok) {
+        return {message: data.message, type: 'success'}
+    }
+    return {message: data.message, type: 'error'}
+        
+}
+
+export const getTournamentList = async () =>{
+    const response = await fetch(`${apiURL}/tournament/list`, {
+        headers: {
+            'ranked-token': localStorage.getItem('ranked-token'),
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+    })
+    if (response.ok) return await response.json()
+    const data = await response.json()
+    return ({message: data.message})
+}
