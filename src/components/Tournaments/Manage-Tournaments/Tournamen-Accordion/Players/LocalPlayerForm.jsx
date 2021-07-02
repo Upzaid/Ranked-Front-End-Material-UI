06583f7a04f73bafd7b0cc6e02f0730d0237f34e
Zaid <zaid_lorea@yahoo.com.mx>
2021-07-01 23:33:54 -0500

    Created form for creating local players

---
+++ src/components/Tournaments/Manage-Tournaments/Tournamen-Accordion/Players/LocalPlayerForm.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react'
+import { Typography, TextField, Grid, Container, Button, Snackbar} from '@material-ui/core'
+import { createPlayer } from '../../../../../functions/players'
+import MuiAlert from '@material-ui/lab/Alert';
+
+
+export default function LocalPlayerForm({getPlayers, tournament}){
+    const [name, setName] = useState()
+    const [message, setMessage] = useState()
+    const [severity, setSeverity] = useState()
+    
+    const submit = async (e)=>{
+        e.preventDefault()
+        const response = await createPlayer(name, tournament)
+        setSeverity(response.type)
+        setMessage(response.message)
+        if (response.type==="success"){
+            getPlayers()
+        }
+    }
+
+    return(
+        <Container>
+            <form onSubmit={(e)=> submit(e)}>
+                <Typography variant='h6'>Add a new player.</Typography>
+                <Grid container>
+                    <Grid item xs={9}>
+                        <TextField 
+                            required 
+                            label="Player Name"
+                            onChange={(e)=>setName(e.target.value)}
+                        />
+                    </Grid>
+                    <Grid item xs={3}>
+                        <Button variant='contained' 
+                                type="submit" 
+                                color="primary"
+                        >  
+                            Add Player
+                        </Button>
+                    </Grid>
+                </Grid>
+            </form>
+            {message 
+            ?  <Snackbar 
+                onClose={()=> setMessage(null)} 
+                autoHideDuration={3000}
+                open
+            >
+                <MuiAlert severity={severity}>{message}</MuiAlert>
+            </Snackbar>
+            : null}
+        </Container>
+    )
+}
