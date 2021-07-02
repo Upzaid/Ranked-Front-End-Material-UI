40f1f04d8c4cb5d7a489d71172ab4c91f2e0e5f8
Zaid <zaid_lorea@yahoo.com.mx>
2021-07-02 17:33:56 -0500

    Created functiond for editing and deliting players

--- src/functions/players.js
+++ src/functions/players.js
@@ -31,10 +31,36 @@ export const createPlayer =async(name, tournament_uuid)=>{
     : {message, type: 'error'} 
 }
 
-export const editPlayer = async() =>{
-
+export const editPlayer = async(name, tournament_uuid, player_uuid) =>{
+    const response = await fetch(`${apiURL}/player/${tournament_uuid}`, {
+        method: 'PUT',
+        headers: {
+            'ranked-token': localStorage.getItem('ranked-token'),
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({name, player_uuid})
+    })
+    const data = await response.json()
+    const {message}  = data
+    return response.ok 
+    ? {message, type: 'success'} 
+    : {message, type: 'error'} 
 }
 
-export const deletePlayer = async() =>{
-    
+export const deletePlayer = async(player_uuid, tournament_uuid) =>{
+    const response = await fetch(`${apiURL}/player/${tournament_uuid}`, {
+        method: 'DELETE',
+        headers: {
+            'ranked-token': localStorage.getItem('ranked-token'),
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({player_uuid})
+    })
+    const data = await response.json()
+    const {message}  = data
+    return response.ok 
+    ? {message, type: 'success'} 
+    : {message, type: 'error'} 
 }
