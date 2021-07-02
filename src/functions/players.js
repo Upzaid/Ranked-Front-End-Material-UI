2dfb067f266df33ca96c9dee3e43601bbbddce4b
Zaid <zaid_lorea@yahoo.com.mx>
2021-07-01 23:35:21 -0500

    Created functions for getting all players in a tournament and creating new players

---
+++ src/functions/players.js
@@ -0,0 +1,40 @@
+const apiURL = process.env.REACT_APP_API_URL
+
+export const getPlayers = async(tournament_uuid)=>{
+    const response = await fetch(`${apiURL}/tournament/registration/${tournament_uuid}`, {
+        headers: {
+            'ranked-token': localStorage.getItem('ranked-token'),
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+    })
+    const data = await response.json()
+    return response.ok 
+    ? data
+    : {message: data.message, type: 'error'} 
+}
+
+export const createPlayer =async(name, tournament_uuid)=>{
+    const response = await fetch(`${apiURL}/player/${tournament_uuid}`, {
+        method: 'POST',
+        headers: {
+            'ranked-token': localStorage.getItem('ranked-token'),
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({name})
+    })
+    const data = await response.json()
+    const {message}  = data
+    return response.ok 
+    ? {message, type: 'success'} 
+    : {message, type: 'error'} 
+}
+
+export const editPlayer = async() =>{
+
+}
+
+export const deletePlayer = async() =>{
+    
+}
