ab4f58c05a3a0357831a2c5ca9800132e3578c43
Zaid <zaid_lorea@yahoo.com.mx>
2021-07-07 23:07:58 -0500

    Created component for displaying a given player's match history in a tournament

---
+++ src/components/Tournaments/Manage-Tournaments/Tournamen-Accordion/Players/PlayerHistoryTable.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Grid, Box, Typography } from '@material-ui/core'
+import { Table, TableRow, TableCell } from '@material-ui/core'
+import EloProgressionGraph from './EloProgressionGraph'
+
+export default function PlayerHistoryTable({matches}) {
+
+    return(
+        <Box >
+            <Typography variant="subtitle1">Match History</Typography>
+            <br />
+            <Grid container  justify='center'>
+                <Box style={{width: 850, height: 400}}>
+                    <EloProgressionGraph 
+                        matches={matches}
+                    />
+                </Box>
+            </Grid>
+            <br />
+            <Table size="small">
+                <TableRow >
+                    <TableCell align='center' variant='head'>Date</TableCell>
+                    <TableCell align='center' variant='head'>Result</TableCell>
+                    <TableCell align='center' variant='head'>Opponent</TableCell>
+                    <TableCell align='center' variant='head'>Rating</TableCell>
+                </TableRow>
+                    {matches.map(match=>{
+                        return (
+                            <TableRow key={match.match_uuid}>
+                                <TableCell align="center">{match.date.split('T')[0]}</TableCell>
+                                <TableCell align="center">{match.player_wins} - {match.opponent_wins}</TableCell>
+                                <TableCell align="center">{match.opponent_name} ({match.opponent_rating})</TableCell>
+                                <TableCell align="center">{match.player_rating}</TableCell>
+                            </TableRow>
+                        )
+                    })}
+            </Table>
+        </Box>
+    )
+}
