14a4becdc0cf9f9f690bf7eb0457c622b1f5bde7
Zaid <zaid_lorea@yahoo.com.mx>
2021-07-12 19:11:12 -0500

    Renamed generic dialog file

---
+++ src/components/GenericDialog.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Dialog, DialogTitle, DialogActions, Button } from '@material-ui/core'
+
+export default function GenericDialog({handleClose, open, onConfirm, onCancel, message, confirmLabel }){
+    return(
+        <Dialog
+            open={open}
+            onClose={()=> handleClose()}
+        >
+            <DialogTitle>{message}</DialogTitle>
+            <DialogActions>
+                <Button color='primary' onClick={()=> onConfirm()}>
+                    {confirmLabel}
+                </Button>
+                <Button color='primary' onClick={()=> onCancel()}>
+                    Cancel
+                </Button>
+            </DialogActions>
+        </Dialog>  
+    )
+}
