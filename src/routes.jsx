96f7ab148541d8f9836a11301ca2083808e8229a
Zaid <zaid_lorea@yahoo.com.mx>
2021-07-14 23:50:40 -0500

    Added theme provider to routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,14 +3,27 @@ import Landing from './components/Landing/Landing'
 import SignIn from './components/Sign-In/SignIn'
 import Register from './components/Register/Register'
 import Home from './components/Home/Home'
+import { createTheme, ThemeProvider } from '@material-ui/core'
+import { indigo } from '@material-ui/core/colors'
+
+const theme = createTheme({
+    palette: {
+        primary: indigo,
+        secondary: {
+            main: '#88abb8'
+        }
+    }
+})
 
 export default function Routes(){
     return(
-        <Router>
-            <Route path='/' exact  component={Landing}/>
-            <Route path='/Home' exact  component={Home}/>
-            <Route path='/sign-in' exact  component={SignIn}/>
-            <Route path='/register' exact  component={Register}/>
-        </Router>
+        <ThemeProvider theme={theme}>
+            <Router>
+                <Route path='/' exact  component={Landing}/>
+                <Route path='/Home' exact  component={Home}/>
+                <Route path='/sign-in' exact  component={SignIn}/>
+                <Route path='/register' exact  component={Register}/>
+            </Router>
+        </ThemeProvider>
     )
 }
