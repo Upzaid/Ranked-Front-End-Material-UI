4925b4a3606b186f47ff0d437873e91dabd39af3
Zaid <zaid_lorea@yahoo.com.mx>
2021-07-17 01:14:08 -0500

    Created functions for sending, accepting and declinig requests

--- src/functions/request.js
+++ src/functions/request.js
@@ -18,15 +18,51 @@ export const sendRequest = async (tournament_uuid)=>{
 }
 
 export const getTournamentRequests = async(tournament_uuid)=>{
+    const response  = await fetch(`${apiURL}/request/${tournament_uuid}`,{
+        headers: {
+            'ranked-token': localStorage.getItem('ranked-token'),
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+    })
 
+    return await response.json()
 }
 
 export const acceptRequest = async (tournament_uuid, user_uuid)=>{
+    const response  = await fetch(`${apiURL}/request/accept/${tournament_uuid}`,{
+        method: 'PUT',
+        headers: {
+            'ranked-token': localStorage.getItem('ranked-token'),
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({user_uuid})
+    })
 
+    const data = await response.json()
+    const {message} = data
+    return response.ok
+    ? {message, type: 'success'}
+    : {message, type: 'error'}
 }
 
 export const declineRequest = async (tournament_uuid, user_uuid)=>{
+    const response  = await fetch(`${apiURL}/request/decline/${tournament_uuid}`,{
+        method: 'PUT',
+        headers: {
+            'ranked-token': localStorage.getItem('ranked-token'),
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({user_uuid})
+    })
 
+    const data = await response.json()
+    const {message} = data
+    return response.ok
+    ? {message, type: 'success'}
+    : {message, type: 'error'}
 }
 
 export const cancelRequest = async (tournament_uuid)=>{
