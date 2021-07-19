b4d11715e430f4f0aa8d567775d932dfe69e80e7
Zaid <zaid_lorea@yahoo.com.mx>
2021-07-18 22:07:34 -0500

    Created user match history tournament accordion componenet

--- src/components/Match-History/UserTournamentAccordion.jsx
+++ src/components/Match-History/UserTournamentAccordion.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import {Accordion, AccordionSummary, AccordionDetails, Grid } from '@material-ui/core'
+import React, { useState, useEffect } from 'react'
+import {Accordion, AccordionSummary, AccordionDetails, Grid, Typography } from '@material-ui/core'
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 import PlayerHistoryTable from '../Tournaments/Manage-Tournaments/Tournament-Accordion/Players/PlayerHistoryTable'
+import { getUserTournamentMatches } from '../../functions/user'
 
 export default function UserTournamentAccordion({tournament}){
     
+    const [matches, setMatches] = useState([])
+
+    useEffect(()=>{
+        fetchMatches()
+    },[])
+
+    const fetchMatches = async ()=>{
+        const response = await getUserTournamentMatches(tournament.tournament_uuid)
+        if(!response.message) setMatches(response)
+    }
+
     return(
         <Accordion>
             <AccordionSummary expandIcon={<ExpandMoreIcon />}>
@@ -13,16 +25,23 @@ export default function UserTournamentAccordion({tournament}){
                         {tournament.tournament_name}
                     </Grid>
                     <Grid item xs={2}>
-                        Matches Played: 
+                        {matches.length} Matches Played
                     </Grid>
                     <Grid item xs={2}>
                         Rating: 
+                        {matches.length > 0 
+                        ? matches[matches.length - 1].player_rating
+                        : 1200
+                        }
                     </Grid>
                 </Grid>
             </AccordionSummary>
             <AccordionDetails style={{textAlign: 'center'}}>
                 <Grid container justify="center">
-                    <PlayerHistoryTable matches={[]}/>
+                    {matches.length < 1
+                    ? <Typography>You have not played any matches in this tournament.</Typography>
+                    :<PlayerHistoryTable matches={matches}/>
+                    }
                 </Grid>
             </AccordionDetails>
         </Accordion>
